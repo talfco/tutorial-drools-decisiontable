eb2fdc19631d58be0e68a2071a20fbb6b23d4cf9
Felix K <talfco@gmail.com>
2018-05-10 12:41:14 +0200

    clean up

--- src/main/java/net/cloudburo/drools/model/Customer.java
+++ src/main/java/net/cloudburo/drools/model/Customer.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Customer {
-
     private CustomerLifeStage lifestage;
     private CustomerAssets assets;
     private List<CustomerNeed> customerNeeds = new ArrayList<>();
